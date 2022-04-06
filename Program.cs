d8f6869c78f335e4e0242bc99e5dbaa2962c13f9
HAKKI AYMAN <82755715+SoulCriminal@users.noreply.github.com>
2022-04-06 18:37:27 +0300

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,45 @@
+﻿
+
+
+Console.WriteLine(Gunler.Cuma);
+Console.WriteLine((int)Gunler.Cuma);//Cuma gününün numerik değerini öğrenme
+
+
+// Enum kullanılarak hava durumu bildirimi yapan kod bloğu
+int sicaklik =32;
+if(sicaklik<= (int)HavaDurumu.Normal)
+{
+    Console.WriteLine("DIşarıya Çıkmak İçin Havanın Biraz Daha Isınmasını Bekle");
+}else if (sicaklik>=(int)HavaDurumu.Sıcak)
+{
+    Console.WriteLine("Dışarıya Çıkmak İçin Çok Sıcak Bir Gün");
+}else if (sicaklik>=(int)HavaDurumu.Normal&&sicaklik<(int)HavaDurumu.ÇokSıcak)
+{
+    Console.WriteLine("Hadi Dışarıya Çıkalım");
+}
+
+
+// Değerleri 1 artar üzerine gelince değerini söterir.
+enum Gunler 
+{
+    Pazartesi,
+    Salı=5,
+    Çarşamba,
+    Perşembe,
+    Cuma,
+    Cumartesi,
+    Pazar
+}
+
+
+
+
+
+
+enum HavaDurumu
+{
+    Soguk = 5,
+    Normal =20,
+    Sıcak=25,
+    ÇokSıcak=30
+}
---
+++ Readme.md
@@ -0,0 +1,14 @@
+# Merhabalar C# Enumaration , kullanım şekilleri ve  açıklamaları içeren kodlarım!
+
+Merhabalar C# ile ilgili öğrenirken hazırladığım  **Enum** kullanımı ile ilgili kodlarımı içeren dosya. 
+
+
+# İçerik
+
+- Enumaration kullanım örnekleri
+- Örnek Kod bloğu
+
+
+
+
+
